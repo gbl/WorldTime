6199a42183a9bab7ec3cae1fb8f0eb332e24a6bd
Guntram Blohm <gbl@guntram.de>
2020-04-23 10:52:25 +0200

    Move injection point so mods that cancel renderStatusEffectOverlay don't break this mod

--- src/main/java/de/guntram/mcmod/worldtime/mixin/PotionEffectsMixin.java
+++ src/main/java/de/guntram/mcmod/worldtime/mixin/PotionEffectsMixin.java
@@ -2,6 +2,8 @@ package de.guntram.mcmod.worldtime.mixin;
 
 import de.guntram.mcmod.worldtime.GuiWorldTime;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,11 +14,14 @@ public abstract class PotionEffectsMixin {
     
     private static GuiWorldTime guiWorldTime;
     
-    @Inject(method="renderStatusEffectOverlay", at=@At("RETURN"))
+    @Inject(method="render", at=@At(
+            value="FIELD", 
+            target="Lnet/minecraft/client/options/GameOptions;debugEnabled:Z", 
+            opcode = Opcodes.GETFIELD, args = {"log=false"}))
     
-    private void onRenderPotionEffects(CallbackInfo ci) {
+    private void onRenderPotionEffects(MatrixStack stack, float f, CallbackInfo ci) {
         if (guiWorldTime==null)
             guiWorldTime=new GuiWorldTime();
-        guiWorldTime.onRenderGameOverlayPost(0);
+        guiWorldTime.onRenderGameOverlayPost(stack, 0);
     }
 }
