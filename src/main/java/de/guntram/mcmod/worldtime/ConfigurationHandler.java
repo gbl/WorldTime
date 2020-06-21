b9316c171f5dca2e494786db80f4bf9494bffcc3
Guntram Blohm <gbl@guntram.de>
2020-06-21 13:53:31 +0200

    Allow config key translations, add coord display

--- src/main/java/de/guntram/mcmod/worldtime/ConfigurationHandler.java
+++ src/main/java/de/guntram/mcmod/worldtime/ConfigurationHandler.java
@@ -23,16 +23,26 @@ public class ConfigurationHandler implements ModConfigurationHandler
     private String RTFormat;
     private String RTPrefix;
     
-    private final String CONF_GT = "Game Time";
-    private final String CONF_X = "Game Time X Percent";
-    private final String CONF_Y = "Game Time Y Percent";
-    private final String PREFIX = "Game Time Prefix";
+    private boolean wantCoords;
+    private int offsetCOLeft, offsetCOTop;
+    private String COFormat, COPrefix;
+    
+    private final String CONF_GT = "worldtime.config.gametime";
+    private final String CONF_GTX = "worldtime.config.gametimex";
+    private final String CONF_GTY = "worldtime.config.gametimey";
+    private final String CONF_GTPREFIX = "worldtime.config.gametimeprefix";
 
-    private final String CONF_RT = "Real Time";
-    private final String CONF_RTX = "Real Time X Percent";
-    private final String CONF_RTY = "Real Time Y Percent";
-    private final String RTFORMAT = "Real Time Format";
-    private final String RTPREFIX = "Real Time Prefix";
+    private final String CONF_RT = "worldtime.config.realtime";
+    private final String CONF_RTX = "worldtime.config.realtimex";
+    private final String CONF_RTY = "worldtime.config.realtimey";
+    private final String CONF_RTFORMAT = "worldtime.config.realtimeformat";
+    private final String CONF_RTPREFIX = "worldtime.config.realtimeprefix";
+    
+    private final String CONF_CO =     "worldtime.config.coords";
+    private final String CONF_COX =     "worldtime.config.coordsx";
+    private final String CONF_COY =     "worldtime.config.coordsy";
+    private final String CONF_COPREFIX ="worldtime.config.coordsprefix";
+    private final String CONF_COFORMAT ="worldtime.config.coordsformat";
 
     public static ConfigurationHandler getInstance() {
         if (instance==null) {
@@ -64,33 +74,59 @@ public class ConfigurationHandler implements ModConfigurationHandler
     public void onConfigChanging(ConfigChangedEvent.OnConfigChangingEvent event) {
         if (event.getModID().equals(WorldTime.MODNAME)) {
             switch (event.getItem()) {
-                case CONF_GT: wantGameTime=(boolean)(Boolean)(event.getNewValue()); break;
-                case CONF_X: offsetLeft=(int)(Integer)(event.getNewValue()); break;
-                case CONF_Y: offsetTop =(int)(Integer)(event.getNewValue()); break;
-                case PREFIX: prefix = (String)(event.getNewValue()); break;
-                case CONF_RT: wantRealTime=(boolean)(Boolean)(event.getNewValue()); break;
-                case CONF_RTX: offsetRTLeft=(int)(Integer)(event.getNewValue()); break;
-                case CONF_RTY: offsetRTTop =(int)(Integer)(event.getNewValue()); break;
-                case RTFORMAT: RTFormat = (String)(event.getNewValue()); break;
-                case RTPREFIX: RTPrefix = (String)(event.getNewValue()); break;
+                case CONF_GT:       wantGameTime=(boolean)(Boolean)(event.getNewValue()); break;
+                case CONF_GTX:      offsetLeft  =(int)(Integer)(event.getNewValue()); break;
+                case CONF_GTY:      offsetTop   =(int)(Integer)(event.getNewValue()); break;
+                case CONF_GTPREFIX: prefix      = (String)(event.getNewValue()); break;
+                
+                case CONF_RT:       wantRealTime=(boolean)(Boolean)(event.getNewValue()); break;
+                case CONF_RTX:      offsetRTLeft=(int)(Integer)(event.getNewValue()); break;
+                case CONF_RTY:      offsetRTTop =(int)(Integer)(event.getNewValue()); break;
+                case CONF_RTFORMAT: RTFormat    = (String)(event.getNewValue()); break;
+                case CONF_RTPREFIX: RTPrefix    = (String)(event.getNewValue()); break;
+                
+                case CONF_CO:       wantCoords  =(boolean)(Boolean)(event.getNewValue()); break;
+                case CONF_COX:      offsetCOLeft=(int)(Integer)(event.getNewValue()); break;
+                case CONF_COY:      offsetCOTop =(int)(Integer)(event.getNewValue()); break;
+                case CONF_COFORMAT: COFormat    = (String)(event.getNewValue()); break;
+                case CONF_COPREFIX: COPrefix    = (String)(event.getNewValue()); break;
             }
         }
     }
     
     private void loadConfig() {
-        config.forget("X Percent");
-        config.forget("Y Percent");
-        config.forget("Prefix");
+        config.migrate("X Percent", CONF_GTX);
+        config.migrate("Y Percent", CONF_GTY);
+        config.migrate("Prefix",    CONF_GTPREFIX);
 
-        wantGameTime=config.getBoolean(CONF_GT, Configuration.CATEGORY_CLIENT, true, "Show ingame time");
-        offsetLeft=config.getInt(CONF_X, Configuration.CATEGORY_CLIENT, 0, 0, 100, "Offset from screen left");
-        offsetTop=config.getInt(CONF_Y, Configuration.CATEGORY_CLIENT, 5, 0, 100, "Offset from screen top");
-        prefix=config.getString(PREFIX, Configuration.CATEGORY_CLIENT, "", "prefix to define colors or similar");
-        wantRealTime=config.getBoolean(CONF_RT, Configuration.CATEGORY_CLIENT, false, "Show real time");
-        offsetRTLeft=config.getInt(CONF_RTX, Configuration.CATEGORY_CLIENT, 0, 0, 100, "Offset from screen left");
-        offsetRTTop=config.getInt(CONF_RTY, Configuration.CATEGORY_CLIENT, 10, 0, 100, "Offset from screen top");
-        RTFormat=config.getString(RTFORMAT, Configuration.CATEGORY_CLIENT, "HH:mm:ss", "Real Time Format (please see Java SimpleDateFormat");
-        RTPrefix=config.getString(RTPREFIX, Configuration.CATEGORY_CLIENT, "", "prefix to define colors or similar");
+        config.migrate("Game Time", CONF_GT);
+        config.migrate("Game Time X Percent", CONF_GTX);
+        config.migrate("Game Time Y Percent", CONF_GTY);
+        config.migrate("Game Time Prefix", CONF_GTPREFIX);
+
+        config.migrate("Real Time", CONF_RT);
+        config.migrate("Real Time X Percent", CONF_RTX);
+        config.migrate("Real Time Y Percent", CONF_RTY);
+        config.migrate("Real Time Prefix", CONF_RTPREFIX);
+        config.migrate("Real Time Format", CONF_RTFORMAT);
+        
+        wantGameTime=config.getBoolean(CONF_GT, Configuration.CATEGORY_CLIENT, true, "worldtime.config.tt.gametime");
+        offsetLeft=config.getInt(CONF_GTX, Configuration.CATEGORY_CLIENT, 0, 0, 100, "worldtime.config.tt.offsetleft");
+        offsetTop=config.getInt(CONF_GTY, Configuration.CATEGORY_CLIENT, 5, 0, 100, "worldtime.config.tt.offsettop");
+        prefix=config.getString(CONF_GTPREFIX, Configuration.CATEGORY_CLIENT, "", "worldtime.config.tt.prefix");
+        
+        wantRealTime=config.getBoolean(CONF_RT, Configuration.CATEGORY_CLIENT, false, "worldtime.config.tt.realtime");
+        offsetRTLeft=config.getInt(CONF_RTX, Configuration.CATEGORY_CLIENT, 0, 0, 100, "worldtime.config.tt.offsetleft");
+        offsetRTTop=config.getInt(CONF_RTY, Configuration.CATEGORY_CLIENT, 10, 0, 100, "worldtime.config.tt.offsettop");
+        RTFormat=config.getString(CONF_RTFORMAT, Configuration.CATEGORY_CLIENT, "HH:mm:ss", "worldtime.config.tt.realtimeformat");
+        RTPrefix=config.getString(CONF_RTPREFIX, Configuration.CATEGORY_CLIENT, "", "worldtime.config.tt.prefix");
+
+        wantCoords=config.getBoolean(CONF_CO, Configuration.CATEGORY_CLIENT, false, "Show real time");
+        offsetCOLeft=config.getInt(CONF_COX, Configuration.CATEGORY_CLIENT, 0, 0, 100, "worldtime.config.tt.offsetleft");
+        offsetCOTop=config.getInt(CONF_COY, Configuration.CATEGORY_CLIENT, 10, 0, 100, "worldtime.config.tt.offsettop");
+        COFormat=config.getString(CONF_COFORMAT, Configuration.CATEGORY_CLIENT, "Position {X}/{Z} Height {Y}", "worldtime.config.tt.coordformat");
+        COPrefix=config.getString(CONF_COPREFIX, Configuration.CATEGORY_CLIENT, "", "worldtime.config.tt.prefix");
+
         if (config.hasChanged())
             config.save();
     }
@@ -110,4 +146,10 @@ public class ConfigurationHandler implements ModConfigurationHandler
     public static int getRealTimeOffsetLeft()   { return getInstance().offsetRTLeft; }
     public static int getRealTimeOffsetTop()    { return getInstance().offsetRTTop; }
     public static String getRealTimePrefix()    { return getInstance().RTPrefix.replace('&', '§'); }
+
+    public static boolean wantCoords()          { return getInstance().wantCoords; }
+    public static String getCoordsFormat()      { return getInstance().COFormat.replace('&', '§'); }
+    public static int getCoordsOffsetLeft()     { return getInstance().offsetCOLeft; }
+    public static int getCoordsOffsetTop()      { return getInstance().offsetCOTop; }
+    public static String getCoordsPrefix()      { return getInstance().COPrefix.replace('&', '§'); }
 }
--- src/main/java/de/guntram/mcmod/worldtime/GuiWorldTime.java
+++ src/main/java/de/guntram/mcmod/worldtime/GuiWorldTime.java
@@ -8,31 +8,25 @@ import net.minecraft.client.util.Window;
 import net.minecraft.client.util.math.MatrixStack;
 
 public class GuiWorldTime {
-    
-    public GuiWorldTime() {
 
+    MinecraftClient minecraft;
+
+    public GuiWorldTime() {
+        minecraft = MinecraftClient.getInstance();
     }
     
     public void onRenderGameOverlayPost(MatrixStack stack, float partialticks) {
-        MinecraftClient minecraft = MinecraftClient.getInstance();
         if (minecraft == null  || minecraft.player == null || minecraft.player.world == null)
             return;
-        Window mainWindow = minecraft.getWindow();
 
         if (ConfigurationHandler.wantGameTime()) {
             int hours=(int) (minecraft.player.world.getTimeOfDay()/1000+6)%24;
             int minutes = (int) ((minecraft.player.world.getTimeOfDay()%1000)*60/1000);
             String clock=ConfigurationHandler.getPrefix()+String.format("%02d:%02d", hours, minutes);
-
-            int xneed = minecraft.textRenderer.getWidth(clock);
-            int yneed = minecraft.textRenderer.fontHeight;
-
-            int xpos = (mainWindow.getScaledWidth()-xneed)*ConfigurationHandler.getOffsetLeft()/100;
-            int ypos = (mainWindow.getScaledHeight()-yneed)*ConfigurationHandler.getOffsetTop()/100;
-
-            minecraft.textRenderer.draw(stack, clock, xpos, ypos, 0xffffff);
+            
+            displayStringAtPercentages(stack, clock, ConfigurationHandler.getOffsetLeft(), ConfigurationHandler.getOffsetTop());
         }
-        
+
         if (ConfigurationHandler.wantRealTime()) {
             String clock;
             try {
@@ -41,14 +35,27 @@ public class GuiWorldTime {
             } catch (IllegalArgumentException ex) {
                 clock = "illegal clock format; google for Java SimpleDateFormat";
             }
-
-            int xneed = minecraft.textRenderer.getWidth(clock);
-            int yneed = minecraft.textRenderer.fontHeight;
-
-            int xpos = (mainWindow.getScaledWidth()-xneed)*ConfigurationHandler.getRealTimeOffsetLeft()/100;
-            int ypos = (mainWindow.getScaledHeight()-yneed)*ConfigurationHandler.getRealTimeOffsetTop()/100;
-
-            minecraft.textRenderer.draw(stack, clock, xpos, ypos, 0xffffff);
+            displayStringAtPercentages(stack, clock, ConfigurationHandler.getRealTimeOffsetLeft(), ConfigurationHandler.getRealTimeOffsetTop());
         }
+        
+        if (ConfigurationHandler.wantCoords()) {
+            String coords = ConfigurationHandler.getCoordsPrefix()+
+                    ConfigurationHandler.getCoordsFormat()
+                        .replace("{X}", String.format("%.1f", minecraft.player.getPos().x))
+                        .replace("{Y}", String.format("%.1f", minecraft.player.getPos().y))
+                        .replace("{Z}", String.format("%.1f", minecraft.player.getPos().z));
+            displayStringAtPercentages(stack, coords, ConfigurationHandler.getCoordsOffsetLeft(), ConfigurationHandler.getCoordsOffsetTop());
+        }
+    }
+    
+    private void displayStringAtPercentages(MatrixStack stack, String string, int xperc, int yperc) {
+        Window mainWindow = minecraft.getWindow();
+        int xneed = minecraft.textRenderer.getWidth(string);
+        int yneed = minecraft.textRenderer.fontHeight;
+
+        int xpos = (mainWindow.getScaledWidth()-xneed)*xperc/100;
+        int ypos = (mainWindow.getScaledHeight()-yneed)*yperc/100;
+
+        minecraft.textRenderer.draw(stack, string, xpos, ypos, 0xffffff);
     }
 }
