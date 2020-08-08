421b10bc44da73d9181879c9525a2e7a7eee755c
Guntram Blohm <gbl@guntram.de>
2020-08-08 08:23:51 +0200

    update to 1.2.3, make each side not require the other side

--- src/main/java/de/guntram/mcmod/worldtime/WorldTime.java
+++ src/main/java/de/guntram/mcmod/worldtime/WorldTime.java
@@ -8,11 +8,18 @@ package de.guntram.mcmod.worldtime;
 import de.guntram.mcmod.GBForgetools.ConfigurationProvider;
 import de.guntram.mcmod.GBForgetools.GuiModOptions;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("worldtime")
 public class WorldTime {
@@ -25,17 +32,23 @@ public class WorldTime {
     private static ConfigurationHandler confHandler;
     
     public WorldTime() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
         bus.addListener(this::init);
     }
 
     public void init(final FMLCommonSetupEvent event) {
-        confHandler = ConfigurationHandler.getInstance();
-        confHandler.load(ConfigurationProvider.getSuggestedFile(MODID));
-        MinecraftForge.EVENT_BUS.register(new ConfigKey());
-        MinecraftForge.EVENT_BUS.register(new GuiWorldTime());
+        if (FMLEnvironment.dist.isClient()) {
+            confHandler = ConfigurationHandler.getInstance();
+            confHandler.load(ConfigurationProvider.getSuggestedFile(MODID));
+            MinecraftForge.EVENT_BUS.register(new ConfigKey());
+            MinecraftForge.EVENT_BUS.register(new GuiWorldTime());
+        } else {
+            System.err.println(MODNAME+" detected a dedicated server. Not doing anything.");
+        }
     }
-    
+
+    @OnlyIn(Dist.CLIENT)
     public static void openConfigScreen() {
         Minecraft.getInstance().displayGuiScreen(new GuiModOptions(null, MODNAME, confHandler));
     }
